9c4108897877ee4d65972d1a636843ce0ca061df
Rajan <109027109+rajan0031@users.noreply.github.com>
2025-11-25 05:06:37 +0000

    added

---
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Message;
+import com.example.demo.repository.MessageRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+    private final MessageRepository repository;
+
+    public MessageController(MessageRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public Message createMessage(@RequestBody Message message) {
+        return repository.save(message);
+    }
+
+    @GetMapping
+    public List<Message> getAllMessages() {
+        return repository.findAll();
+    }
+
+    // @GetMapping("/server")
+    // public string helloServer(){
+    //     return "hey server is running";
+    // }
+}
